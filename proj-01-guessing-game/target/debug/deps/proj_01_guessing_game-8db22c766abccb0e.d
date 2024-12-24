ee1c865aaafb8ea7dc64f127559fa697239cfebe
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-12-24 15:58:47 +0100

    feat: read from input

---
+++ proj-01-guessing-game/target/debug/deps/proj_01_guessing_game-8db22c766abccb0e.d
@@ -0,0 +1,5 @@
+/Users/maciejka/learn/learn-rust/proj-01-guessing-game/target/debug/deps/proj_01_guessing_game-8db22c766abccb0e: src/main.rs
+
+/Users/maciejka/learn/learn-rust/proj-01-guessing-game/target/debug/deps/proj_01_guessing_game-8db22c766abccb0e.d: src/main.rs
+
+src/main.rs:
---
+++ proj-01-guessing-game/target/debug/proj-01-guessing-game.d
@@ -0,0 +1,1 @@
+/Users/maciejka/learn/learn-rust/proj-01-guessing-game/target/debug/proj-01-guessing-game: /Users/maciejka/learn/learn-rust/proj-01-guessing-game/src/main.rs
